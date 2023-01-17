dfeecb8aee92e4a2b47bbddc17813758cfcbdb15
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-17 14:25:43 +0100

    Fixed the issue from Firebase.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: [
+    'firebase',
+    'firebase/app',
+    'firebase/auth',
+    'firebase/firestore',
+    'firebase/storage',
+    'firebase/analytics',
+    ],
+    },
   plugins: [react(), tsconfigPaths()],
 })
