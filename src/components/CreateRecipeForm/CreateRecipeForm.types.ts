40ad713e75f537644ec7a8e166a77541c405fb94
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-18 20:04:16 +0100

    Moved the Inputs type to the global type-file and renamed it.

--- src/components/CreateRecipeForm/CreateRecipeForm.types.ts
+++ src/components/CreateRecipeForm/CreateRecipeForm.types.ts
@@ -1,19 +1,3 @@
-// export interface Ingredient {
-// 	amount: number,
-// 	unit: string,
-// 	ingredient: string,
-// }
-
-export interface Inputs {
-	title: string,
-	portions: number,
-	cookingTime: number,
-	numberOfIngredients: number,
-	ingredients: {amount: number, unit: string, ingredient: string}[],
-	tags: string[],
-	instructions: string,
-}
-
 export interface CreateRecipeFormProps {
 	className?: string,
 }
