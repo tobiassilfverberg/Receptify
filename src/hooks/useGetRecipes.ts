af3428b63d59cb6ca7f5ea5d4d97fc28c53e09d2
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-18 20:07:04 +0100

    Also uses the new global Recipe interface but also set a type for the snapshot doc.

--- src/hooks/useGetRecipes.ts
+++ src/hooks/useGetRecipes.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react"
-import { collection, limit, orderBy, onSnapshot, query } from 'firebase/firestore'
+import { collection, DocumentData, limit, orderBy, onSnapshot, query } from 'firebase/firestore'
 import { db } from "@firebase/index"
 
 const useGetRecipes = () => {
-	const [recipes, setRecipes] = useState<any>()
+	const [recipes, setRecipes] = useState<Recipe[] | null>(null)
 	const [loading, setLoading] = useState<boolean>(false)
 
 	useEffect(() => {
@@ -11,11 +11,13 @@ const useGetRecipes = () => {
 		const queryRef = query(collection(db, 'recipes'), orderBy('createdAt', 'desc'), limit(10))
 
 		const unsubscribe = onSnapshot(queryRef, (snapshot) => {
-			const docs = snapshot.docs.map(doc => {
+			const docs = snapshot.docs.map((doc: DocumentData) => {
 				return {
 					...doc.data(),
 				}
 			})
+
+			console.log(docs)
 
 			setRecipes(docs)
 			setLoading(false)
