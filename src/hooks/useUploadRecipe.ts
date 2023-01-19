9f53b3432e01e5f6c33151f157ea08888b11c94e
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-19 15:08:22 +0100

    Moved the logic of uploading a recipe to a custom hook. Now also takes an image.

---
+++ src/hooks/useUploadRecipe.ts
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
+import { db, storage } from '@firebase/index'
+
+const useUploadRecipe = () => {
+	const [error, setError] = useState()
+
+	const uploadRecipe = ( data: Recipe, imageFile: File ) => {
+		const prefix = Date.now()
+
+		if(!imageFile) {
+			return null
+		}
+
+		const fileRef = ref(storage, `recipes/${prefix}-${imageFile?.name}`)
+
+		const uploadTask = uploadBytesResumable(fileRef, imageFile)
+
+		uploadTask.on('state_changed', (uploadTaskSnapshot) => {
+			// setUploadProgress(Math.round((uploadTaskSnapshot.bytesTransferred / uploadTaskSnapshot.totalBytes) * 100))
+		}, (e: any) => {
+			console.log("Gick inte", e)
+			setError(e)
+		}, async () => {
+			const url = await getDownloadURL(fileRef)
+
+			try {
+				await addDoc(collection(db, 'recipes'), {
+					title: data.title,
+					imageRef: url,
+					portions: Number(data.portions),
+					cookingTime: Number(data.cookingTime),
+					numberOfIngredients: Number(data.numberOfIngredients),
+					ingredients: data.ingredients,
+					tags: data.tags,
+					instructions: data.instructions,
+					createdAt: Timestamp.now(),
+				})
+			} catch (err: any) {
+				setError(err)
+			}
+		})
+	}
+
+	return {
+		uploadRecipe,
+		error
+	}
+}
+
+export default useUploadRecipe
