160b59f16c5ca254735d8a3740ccba6eade19d86
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-20 11:30:36 +0100

    Removed heading and weird import.

--- src/pages/RegisterUserPage.tsx
+++ src/pages/RegisterUserPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Container from 'react-bootstrap/Container'
 
 import RegisterUserForm from '@components/RegisterUserForm'
@@ -7,12 +5,7 @@ import RegisterUserForm from '@components/RegisterUserForm'
 const RegisterUserPage = () => {
 	return (
 		<Container>
-			<h3>Registrera dig som användare</h3>
-
 			<RegisterUserForm />
-
-			<p>Har du redan ett konto?</p>
-			<p>Klicka här för att logga in</p>
 		</Container>
 	)
 }
