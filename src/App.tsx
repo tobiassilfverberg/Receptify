0f51023e21bbf21baf2909b253b75a4c4ca185d4
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-20 11:28:30 +0100

    Included the navbar component and routes for logout.

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { Routes, Route } from 'react-router-dom'
 import CreateRecipe from "@pages/CreateRecipe"
 import LandingPage from '@pages/LandingPage'
 import LoginUserPage from '@pages/LoginUserPage'
+import LogoutPage from '@pages/LogoutPage'
 import RegisterUserPage from '@pages/RegisterUserPage'
 import RequireAuth from '@components/RequireAuth'
+import Navigation from '@components/Navigation'
 import '@styles/App.scss'
 
 function App() {
 	return (
 		<div className="App">
+			<Navigation />
+
 			<Routes>
 				{/* Public routes */}
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/loggain" element={<LoginUserPage />} />
+				<Route path="/loggaut" element={<LogoutPage />} />
 				<Route path="/registrera" element={<RegisterUserPage />} />
 
 				{/* Protected routes */}
