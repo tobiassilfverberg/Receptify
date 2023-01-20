d7aba1e3c36ab6e7d43094636203480e33dfbb66
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-20 22:35:04 +0100

    You can now filter via search of tags.

--- src/components/ShowRecipes/ShowRecipes.tsx
+++ src/components/ShowRecipes/ShowRecipes.tsx
@@ -1,28 +1,113 @@
+import { useEffect, useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import { SubmitHandler, useForm, FieldValues } from 'react-hook-form'
 import RecipeCard from './RecipeCard'
-import { Container } from 'react-bootstrap/'
-import useGetRecipes from '../../hooks/useGetRecipes'
+import { Container, Button, ButtonGroup } from 'react-bootstrap'
+import useGetRecipes from '@hooks/useGetRecipes'
 import styles from './ShowRecipe.module.scss'
 
+const tagArray = ["Alla", "Torsk", "Fläsk", "Nöt", "Pasta", "Potatis", "Ris", "Korv", "Lax", "Vegetariskt", "Köttfärs", "Skaldjur", "Frukost", "Lunch", "Mellanmål", "Förrätt", "Varmrätt", "Efterrätt", "Bakverk"]
+
 const ShowRecipes = () => {
-	const { recipes, loading } = useGetRecipes()
+	const [searchInput, setSearchInput] = useState<string>('')
+	const [searchTag, setSearchTag] = useState<string>('')
+	const [filteredRecipes, setFilteredRecipes] = useState<Recipe[] | null | undefined>(null)
+	const { getRecipes, recipes } = useGetRecipes()
+
+	const { register, handleSubmit, formState: { errors }, watch } = useForm()
+	const watchTag = watch('tags')
+
+	useEffect(() => {
+		const subscription = watch((value) => {
+			setSearchTag(value.tags)
+		})
+
+		return () => subscription.unsubscribe()
+	}, [watch])
+
+	// const onSubmit: SubmitHandler<FieldValues> = (data: any) => {
+	// 	getRecipes(data.tags)
+	// }
+
+	const onSearch: SubmitHandler<FieldValues> = (data: any) => {
+		const filtered = recipes?.filter((recipe: Recipe) => {
+			return (
+				recipe.title.toLowerCase().includes(data.searchName.toLowerCase())
+			)
+		})
+
+		setFilteredRecipes(filtered)
+	}
+
+	useEffect(() => {
+		getRecipes('Alla')
+	}, [])
 
 	return (
 		<Container className={`${styles.root__main}`}>
 			<h4>Senast tillagda recept</h4>
 
-			{loading && (
+			<Form
+				// onSubmit={handleSubmit(onSubmit)}
+				className={`${styles.root__searchForms}`}
+			>
+				<Form.Label>Välj tag</Form.Label>
+				<Form.Select
+					{...register('tags', {
+						required: false,
+					})}
+					className="w-50"
+				>
+					{tagArray.map((tag) => (
+						<option key ={`${tag}`} value={`${tag}`}>{`${tag}`}</option>
+					))}
+				</Form.Select>
+				<Button type="submit" className='mt-2'>Välj</Button>
+			</Form>
+
+			<Form onSubmit={handleSubmit(onSearch)} className={`${styles.root__searchForms}`}>
+					<Form.Label>Sök efter titel</Form.Label>
+					<Form.Control
+						{...register('searchName')}
+						type="text"
+						onChange={(e) => setSearchInput(e.target.value)}
+						className="w-50"
+					/>
+					<ButtonGroup>
+						<Button type="submit" className='mt-2'>Sök</Button>
+						<Button type="reset" className="mt-2" variant="warning" onClick={() => {
+							setSearchInput('')
+						setFilteredRecipes(null)
+						}}>Återställ</Button>
+					</ButtonGroup>
+			</Form>
+
+			{/* {loading && (
 				<p>Laddar recept</p>
+			)} */}
+
+			{(searchInput && filteredRecipes?.length === 0) && (
+				<p>Prova en annan sökning</p>
 			)}
 
-			{recipes && (
+			{filteredRecipes? (
 				<div className={`${styles.root__wrapper}`}>
 
-					{recipes.map((recipe: Recipe) => (
-						<RecipeCard recipe={recipe} />
+					{filteredRecipes?.map((recipe: Recipe) => (
+						<RecipeCard key={recipe.title} recipe={recipe} />
+					))}
+
+				</div>
+			) : (
+				<div className={`${styles.root__wrapper}`}>
+
+					{recipes?.map((recipe: Recipe) => (
+						<RecipeCard key={recipe.title} recipe={recipe} />
 					))}
 
 				</div>
 			)}
+
 		</Container>
 	)
 }
