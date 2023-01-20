c7f5de4b4ad41152d54cc888a9be95238d0939c7
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-20 11:32:22 +0100

    Put the form inside a card to make it extra pretty :).

--- src/components/RegisterUserForm/RegisterUserForm.tsx
+++ src/components/RegisterUserForm/RegisterUserForm.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import Alert from 'react-bootstrap/Alert'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
+import { Alert, Button, Card, Container, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 import { SubmitHandler, useForm } from 'react-hook-form'
 
@@ -11,6 +9,8 @@ import { User } from './RegisterUserForm.types'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { collection, addDoc } from 'firebase/firestore'
 import { auth, db } from '@firebase/index'
+
+import styles from './RegisterUserForm.module.scss'
 
 const RegisterUserForm = () => {
 	const [error, setError] = useState<string>()
@@ -43,68 +43,81 @@ const RegisterUserForm = () => {
 	}
 
 	return (
-		<Container className="mt-3 mb-3 text-light">
-			<h3>Add admin: </h3>
-			<Form onSubmit={handleSubmit(onSubmit)}>
-				<Form.Group id="email" className="mb-1">
-					<Form.Control
-						{...register("email", {
-							required: "Du måste ange en email",
-							minLength: {
-								value: 6,
-								message: "Du måste ange en giltig email"
-							}
-						})}
-						placeholder="Email"
-						type="email"
-					/>
-				</Form.Group>
+		<Container className={`${styles.root__container}`}>
+			<Card className='w-75'>
+				<Card.Body>
+					<Card.Title className='mb-3'>
+						Registrera ett konto
+					</Card.Title>
 
-				<Form.Group id="username" className="mb-1">
-					<Form.Control
-						{...register("username", {
-							required: "Du måste ange ett namn",
-							minLength: {
-								value: 3,
-								message: "Du måste ange en normalt namn"
-							}
-						})}
-						placeholder="Namn"
-						type="text"
-					/>
-				</Form.Group>
+					<Form onSubmit={handleSubmit(onSubmit)}>
+						<Form.Group id="email" className="mb-1">
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								{...register("email", {
+									required: "Du måste ange en email",
+									minLength: {
+										value: 6,
+										message: "Du måste ange en giltig email"
+									}
+								})}
+								type="email"
+							/>
+						</Form.Group>
 
-				<Form.Group id="password" className="mb-1">
-					<Form.Control
-						{...register("password", {
-							required: "Du måste ange ett lösenord",
-							minLength: {
-								value: 5,
-								message: "Ange minst 5 tecken i ditt lösenord"
-							}
-						})}
-						placeholder="Lösenord"
-						type="password"
-					/>
-				</Form.Group>
+						<Form.Group id="username" className="mb-1">
+							<Form.Label>Användarnamn</Form.Label>
+							<Form.Control
+								{...register("username", {
+									required: "Du måste ange ett namn",
+									minLength: {
+										value: 3,
+										message: "Du måste ange en normalt namn"
+									}
+								})}
+								type="text"
+							/>
+						</Form.Group>
 
-				<Form.Group id="password-confirm" className="mb-1">
-					<Form.Control
-						{...register("passwordConfirm", {
-							required: "Du måste matcha ditt lösenord",
-							minLength: {
-								value: 5,
-								message: "Du måste ange samma lösenord"
-							}
-						})}
-						placeholder="Bekräfta lösenord"
-						type="password"
-					/>
-				</Form.Group>
+						<Form.Group id="password" className="mb-1">
+							<Form.Label>Lösenord</Form.Label>
+							<Form.Control
+								{...register("password", {
+									required: "Du måste ange ett lösenord",
+									minLength: {
+										value: 5,
+										message: "Ange minst 5 tecken i ditt lösenord"
+									}
+								})}
+								type="password"
+							/>
+						</Form.Group>
 
-				<Button disabled={loadingCreateAdmin} type="submit" className="mt-1">Create Admin</Button>
-			</Form>
+						<Form.Group id="password-confirm" className="mb-1">
+							<Form.Label>Bekräfta lösenord</Form.Label>
+							<Form.Control
+								{...register("passwordConfirm", {
+									required: "Du måste matcha ditt lösenord",
+									minLength: {
+										value: 5,
+										message: "Du måste ange samma lösenord"
+									}
+								})}
+								type="password"
+							/>
+						</Form.Group>
 
+						<Button disabled={loadingCreateAdmin} type="submit" className="mt-1">Skapa konto</Button>
+					</Form>
+
+					<div className='text-center mt-3'>
+						<Link to='/loggain'>
+							Har du redan ett konto? Klicka här för att logga in
+						</Link>
+					</div>
+
+				</Card.Body>
+			</Card>
 			{/* {errors.title && (<Alert className="mt-2" variant="danger">{errors.title.message}</Alert>)} */}
 			{error && (<Alert className="mt-2" variant="danger">{error}</Alert>)}
 		</Container>
