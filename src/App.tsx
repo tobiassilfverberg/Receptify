dba5a888eefd4430fcde1a66312cd79e1ec5ad95
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-20 10:31:08 +0100

    Made createrecipe a protected route, so that only logged in users can create recipes.

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,24 @@ import CreateRecipe from "@pages/CreateRecipe"
 import LandingPage from '@pages/LandingPage'
 import LoginUserPage from '@pages/LoginUserPage'
 import RegisterUserPage from '@pages/RegisterUserPage'
+import RequireAuth from '@components/RequireAuth'
 import '@styles/App.scss'
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
+				{/* Public routes */}
 				<Route path="/" element={<LandingPage />} />
 				<Route path="/loggain" element={<LoginUserPage />} />
 				<Route path="/registrera" element={<RegisterUserPage />} />
-				<Route path="/skaparecept" element={<CreateRecipe />} />
+
+				{/* Protected routes */}
+				<Route path="/skaparecept" element={
+					<RequireAuth>
+						<CreateRecipe />
+					</RequireAuth>
+				} />
 			</Routes>
 		</div>
 	)
