148d3d607c705a5ebf39fa39d48ca06260cb1a07
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-21 15:15:11 +0100

    Made the card a link.

--- src/components/ShowRecipes/RecipeCard.tsx
+++ src/components/ShowRecipes/RecipeCard.tsx
@@ -1,10 +1,11 @@
 import Card from 'react-bootstrap/Card'
 import styles from './ShowRecipe.module.scss'
 import { RecipeCardProps } from './RecipeCard.types'
+import { Link } from 'react-router-dom'
 
 const RecipeCard = ({ recipe }: RecipeCardProps) => {
 	return (
-		<Card key={recipe.imageRef} className={`${styles.root__recipeCard}`}>
+		<Card as={Link} to={`recept/${recipe.title}`} key={recipe.imageRef} className={`${styles.root__recipeCard}`}>
 			<Card.Img className={`${styles.root__cardImg}`} variant="top" src={recipe.imageRef ? recipe.imageRef : "https://via.placeholder.com/50"} />
 			<Card.Body>
 				<Card.Title className="text-center">{recipe.title}</Card.Title>
