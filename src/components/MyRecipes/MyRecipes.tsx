3e46e88890b3f52b70011db762f1785ff4982fab
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2023-01-22 17:57:50 +0100

    'MyRecipes' component which shows the loggedin users recipes.

---
+++ src/components/MyRecipes/MyRecipes.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react'
+import { Container, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import useGetMyRecipes from '@hooks/useGetMyRecipes'
+import styles from './MyRecipes.module.scss'
+
+const MyRecipes = () => {
+	const { getRecipes, recipes } = useGetMyRecipes()
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		getRecipes()
+	}, [])
+
+	return (
+		<Container className={`${styles.root}`}>
+			<h3>Mina recept</h3>
+			<ListGroup>
+				{recipes?.map((recipe: Recipe) => (
+					<ListGroupItem onClick={() => {
+					navigate(`/recept/${recipe.title}`)
+					}}>
+					<div className={`${styles.root__flex}`}>
+						<div>
+							<h5>{recipe.title}</h5>
+							<div>{recipe.tags.join(', ')}</div>
+							<div>{recipe.portions} portioner</div>
+							<div>{recipe.cookingTime} minuter</div>
+						</div>
+						<img src={recipe.imageRef} />
+					</div>
+					</ListGroupItem>
+				))}
+			</ListGroup>
+		</Container>
+	)
+}
+
+export default MyRecipes
